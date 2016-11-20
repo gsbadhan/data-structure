278ec86b9795efd5662ffece78226f9291c7edf8
gsbadhan <gurpreet.badhan.28@gmail.com>
2016-11-20 19:26:10 +0530

    infixtopostfix

---
+++ src/com/infixToPostfix/InfixToPostfixExpression.java
@@ -0,0 +1,86 @@
+package com.infixToPostfix;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class InfixToPostfixExpression {
+	private static char OPEN_BRACKET='(';
+	private static char CLOSE_BRACKET=')';
+	private static Stack<Character> stack = new Stack<>();
+	private static Map<Character, Integer> operatorPriority = new HashMap<>();
+	private static String postFixExp = "";
+
+	static {
+		operatorPriority.put('^', 3);
+		operatorPriority.put('*', 2);
+		operatorPriority.put('/', 2);
+		operatorPriority.put('+', 1);
+		operatorPriority.put('-', 1);
+	}
+
+	
+
+	private static void infixToPostfix(String exp) {
+		stack.push(OPEN_BRACKET);
+		char tokens[] = exp.toCharArray();
+		for (char c : tokens) {
+			if (isOperator(c)) {
+				popHigherOperator(c);
+				stack.push(c);
+			} else if (c == OPEN_BRACKET) {
+				stack.push(c);
+			} else if (c == CLOSE_BRACKET) {
+				popUptoOpenBracket();
+			} else {
+				postFixExp += c;
+			}
+		}
+		stack.add(CLOSE_BRACKET);
+		//
+
+		//Empty all Stack
+		while (!stack.isEmpty()) {
+			char c = stack.pop();
+			if (c != OPEN_BRACKET && c != CLOSE_BRACKET) {
+				postFixExp += c;
+			}
+		}
+	}
+
+	private static void popUptoOpenBracket() {
+		while (stack.peek() != OPEN_BRACKET) {
+			char c = stack.pop();
+			if (c != OPEN_BRACKET && c != CLOSE_BRACKET) {
+				postFixExp += c;
+			}
+		}
+		stack.pop();
+	}
+
+	private static void popHigherOperator(char c) {
+		if (isOperator(stack.peek()) && isHigherOrSame(stack.peek(), c)) {
+			postFixExp += stack.pop();
+			popHigherOperator(c);
+		}
+	}
+
+	private static boolean isHigherOrSame(char oldc, char newc) {
+		int oldcc = operatorPriority.get(oldc);
+		int newcc = operatorPriority.get(newc);
+		return (newcc <= oldcc ? true : false);
+	}
+
+	private static boolean isOperator(char c) {
+		return operatorPriority.containsKey(c);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		String exp = "A+(B*C-(D/E^F)*G)*H)";
+		infixToPostfix(exp);
+		System.out.println("postexp:" + postFixExp);
+	}
+
+}
